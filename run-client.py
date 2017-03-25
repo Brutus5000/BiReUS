b80cd0540c76e40eb0dc79df69fa5c37b592500b
Christian Schmidt <c-schmidt@gmx.eu>
2017-03-25 15:30:49 +0100

    add encoding everywhere

--- run-client.py
+++ run-client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# coding=utf-8
 """Bidirectional Repository Update Service - client component"""
 import argparse
 import asyncio
--- run-server.py
+++ run-server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# coding=utf-8
 """Bidirectional Repository Update Service - server component"""
 import argparse
 import logging
