23542f5a1e94b9f130e5ff97e4c1cc00aa03b8d5
Christian Schmidt <c-schmidt@gmx.eu>
2017-03-25 13:06:43 +0100

    use global logger

--- server/compare_task.py
+++ server/compare_task.py
@@ -12,11 +12,10 @@ from shared import crc32_from_file
 from shared.diff_head import DiffHead
 from shared.diff_item import DiffItem
 
+logger = logging.getLogger(__name__)
 
 class CompareTask(object):
     def __init__(self, absolute_path: Path, name: str, base: str, target: str):
-        self.logger = logging.getLogger('server.CompareTask')
-
         self._absolute_path = absolute_path
         self.name = name
         self.base = base
@@ -27,7 +26,7 @@ class CompareTask(object):
         self._deltapath = absolute_path.joinpath(self.base, '.delta_to', self.target)
 
     def generate_diff(self, write_deltafile: bool = True) -> DiffHead:
-        self.logger.debug('Generating %s delta `%s` -> `%s`', self.name, self.base, self.target)
+        logger.debug('Generating %s delta `%s` -> `%s`', self.name, self.base, self.target)
 
         self._deltapath.mkdir(parents=True, exist_ok=True)
 
@@ -51,7 +50,7 @@ class CompareTask(object):
         return bireus_head
 
     def _compare_directory(self, relative_path: Path) -> DiffItem:
-        self.logger.debug("_compare_directory for `%s`", relative_path)
+        logger.debug("_compare_directory for `%s`", relative_path)
 
         basepath = self._basepath.joinpath(relative_path)  # type: Path
         targetpath = self._targetpath.joinpath(relative_path)  # type: Path
@@ -93,7 +92,7 @@ class CompareTask(object):
         return result_diff
 
     def _compare_file(self, relative_path: Path, file_path: str) -> DiffItem:
-        self.logger.debug("_compare_file for `%s` in `%s`", file_path, relative_path)
+        logger.debug("_compare_file for `%s` in `%s`", file_path, relative_path)
 
         result_diff = DiffItem(iotype='file',
                                name=file_path,
@@ -139,7 +138,7 @@ class CompareTask(object):
                 with zipfile.ZipFile(str(targetpath)) as targetZip:
                     targetZip.extractall(str(temp_targetpath))
 
-                self.logger.debug("zipdelta required for `%s`", file_path)
+                logger.debug("zipdelta required for `%s`", file_path)
                 zip_diff = CompareTask(temp_abspath, self.name, self.base, self.target).generate_diff(False)
                 copy_folder(temp_deltapath, self._absolute_path.joinpath(self._deltapath, file_path))
 
@@ -153,7 +152,7 @@ class CompareTask(object):
         return result_diff
 
     def _add_directory(self, relative_path: Path) -> None:
-        self.logger.debug("_add_directory for `%s`", relative_path)
+        logger.debug("_add_directory for `%s`", relative_path)
 
         targetpath = self._targetpath.joinpath(relative_path)
         deltapath = self._deltapath.joinpath(relative_path)
