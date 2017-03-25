93d11fc7423b8304616ff045332dbe369a9bcb3c
Christian Schmidt <c-schmidt@gmx.eu>
2017-03-25 15:10:54 +0100

    implemented incremental and instant strategy algorithms

---
+++ server/patch_strategy.py
@@ -0,0 +1,59 @@
+import abc
+from typing import List, Tuple
+
+from networkx import Graph
+
+
+class AbstractStrategy(abc.ABC):
+    """
+    Abstract strategy class that determines, which patches need to be created on a new version
+    """
+
+    @abc.abstractclassmethod
+    def get_required_patches(self, version_graph: Graph, last_version: str, new_version: str) -> List[Tuple[str, str]]:
+        """
+        Calculates which patches need to be generated
+        :param version_graph: graph of all versions - Attention! gets manipulated!
+        :param last_version: predecessor of new_version
+        :param new_version: the new version
+        :return: a list of version-pairs for which patches need to be created
+        """
+        pass
+
+
+class IncrementalStrategy(AbstractStrategy):
+    def __init__(self, bidirectional: bool = True):
+        self.bidirectional = bidirectional
+
+    def get_required_patches(self, version_graph: Graph, last_version: str, new_version: str) -> List[Tuple[str, str]]:
+        required_patches = [(last_version, new_version)]
+
+        version_graph.add_node(new_version)
+        version_graph.add_edge(last_version, new_version)
+
+        if self.bidirectional:
+            version_graph.add_edge(new_version, last_version)
+            required_patches.append((new_version, last_version))
+
+        return required_patches
+
+
+class InstantStrategy(AbstractStrategy):
+    def __init__(self, bidirectional: bool = True):
+        self.bidirectional = bidirectional
+
+    def get_required_patches(self, version_graph: Graph, last_version: str, new_version: str) -> List[Tuple[str, str]]:
+        required_patches = []
+
+        versions = list(version_graph)
+        version_graph.add_node(new_version)
+
+        for version in versions:
+            required_patches.append((version, new_version))
+            version_graph.add_edge(version, new_version)
+
+            if self.bidirectional:
+                required_patches.append((new_version, version))
+                version_graph.add_edge(new_version, version)
+
+        return required_patches
---
+++ tests/test_patch_strategy.py
@@ -0,0 +1,98 @@
+import networkx
+import pytest
+
+from server.patch_strategy import IncrementalStrategy, InstantStrategy
+
+
+@pytest.fixture
+def incremental_bi_three_version_graph() -> networkx.Graph:
+    version_graph = networkx.DiGraph()
+
+    version_graph.add_nodes_from(["v1", "v2", "v3"])
+    version_graph.add_edge("v1", "v2")
+    version_graph.add_edge("v2", "v1")
+    version_graph.add_edge("v2", "v3")
+    version_graph.add_edge("v3", "v2")
+
+    return version_graph
+
+
+@pytest.fixture
+def incremental_uni_three_version_graph():
+    version_graph = networkx.DiGraph()
+
+    version_graph.add_nodes_from(["v1", "v2", "v3"])
+    version_graph.add_edge("v1", "v2")
+    version_graph.add_edge("v2", "v3")
+
+    return version_graph
+
+
+def test_incremental_bi_3_to_4(incremental_bi_three_version_graph):
+    version_graph = incremental_bi_three_version_graph  # type: networkx.Graph
+
+    strategy = IncrementalStrategy()
+    patches = strategy.get_required_patches(version_graph, "v3", "v4")
+
+    assert len(patches) == 2
+    assert patches[0][0] == "v3"
+    assert patches[0][1] == "v4"
+    assert patches[1][0] == "v4"
+    assert patches[1][1] == "v3"
+
+    assert not version_graph.has_edge("v1", "v4")
+    assert not version_graph.has_edge("v2", "v4")
+    assert version_graph.has_edge("v3", "v4")
+    assert not version_graph.has_edge("v4", "v1")
+    assert not version_graph.has_edge("v4", "v2")
+    assert version_graph.has_edge("v4", "v3")
+
+
+def test_instant_bi_3_to_4(incremental_bi_three_version_graph):
+    version_graph = incremental_bi_three_version_graph  # type: networkx.Graph
+
+    strategy = InstantStrategy()
+    patches = strategy.get_required_patches(version_graph, "v3", "v4")
+
+    assert len(patches) == 6
+
+    assert version_graph.has_edge("v1", "v4")
+    assert version_graph.has_edge("v2", "v4")
+    assert version_graph.has_edge("v3", "v4")
+    assert version_graph.has_edge("v4", "v1")
+    assert version_graph.has_edge("v4", "v2")
+    assert version_graph.has_edge("v4", "v3")
+
+
+def test_incremental_uni_3_to_4(incremental_uni_three_version_graph):
+    version_graph = incremental_uni_three_version_graph  # type: networkx.Graph
+
+    strategy = IncrementalStrategy(False)
+    patches = strategy.get_required_patches(version_graph, "v3", "v4")
+
+    assert len(patches) == 1
+    assert patches[0][0] == "v3"
+    assert patches[0][1] == "v4"
+
+    assert not version_graph.has_edge("v1", "v4")
+    assert not version_graph.has_edge("v2", "v4")
+    assert version_graph.has_edge("v3", "v4")
+    assert not version_graph.has_edge("v4", "v1")
+    assert not version_graph.has_edge("v4", "v2")
+    assert not version_graph.has_edge("v4", "v3")
+
+
+def test_instant_uni_3_to_4(incremental_uni_three_version_graph):
+    version_graph = incremental_uni_three_version_graph  # type: networkx.Graph
+
+    strategy = InstantStrategy(False)
+    patches = strategy.get_required_patches(version_graph, "v3", "v4")
+
+    assert len(patches) == 3
+
+    assert version_graph.has_edge("v1", "v4")
+    assert version_graph.has_edge("v2", "v4")
+    assert version_graph.has_edge("v3", "v4")
+    assert not version_graph.has_edge("v4", "v1")
+    assert not version_graph.has_edge("v4", "v2")
+    assert not version_graph.has_edge("v4", "v3")
